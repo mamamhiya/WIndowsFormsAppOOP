220765d6817614502552942ca0c7cefb40511ce6
Woramin-130 <sukuyyo@gmail.com>
2026-01-07 10:55:43 +0700

    3

--- WIndowsFormsAppOOP/Form1.cs
+++ WIndowsFormsAppOOP/Form1.cs
@@ -10,16 +10,13 @@ namespace WIndowsFormsAppOOP
         private void button1_Click(object sender, EventArgs e)
         {
             TextShape triangel = new TextShape();
-            triangel.letters = "x";
-            triangel.height = 9;
-            triangel.DrawTriangle();
+            triangel.DrawTriangle(9,"X");
             textBox1.Text = triangel.result;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            TextShape rectangle = new TextShape(5,4);
-            rectangle.letters = "O";
+            TextShape rectangle = new TextShape(5,5,"O");
             rectangle.DrawRectangle();
             textBox1.Text = rectangle.result;
         }
--- WIndowsFormsAppOOP/TextShape.cs
+++ WIndowsFormsAppOOP/TextShape.cs
@@ -47,6 +47,21 @@ namespace WIndowsFormsAppOOP
             this.result = result;
 
         }
+        public void DrawTriangle(double Height,string Letter)
+        {
+            height = Height;
+            letters = Letter;
+            string result = "";
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    result += this.letters;
+                }
+                result += Environment.NewLine;
+            }
+            this.result = result;
+        }
         public void DrawRectangle()
         {
             string result = "";
