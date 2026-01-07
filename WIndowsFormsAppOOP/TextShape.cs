8e1de2fc7acdab6b8e4d1cca817c794aa20c2b7c
Woramin-130 <sukuyyo@gmail.com>
2026-01-07 10:04:57 +0700

    Add project files.

---
+++ WIndowsFormsAppOOP/TextShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIndowsFormsAppOOP
+{
+    internal class TextShape
+    {
+        private double width;
+        private double height;
+        public TextShape(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
